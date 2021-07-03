7c76e9f07c86d7d250a4e999c2a79a46008aea2c
navierhsiao <oaishreivan@gmail.com>
2021-07-04 00:14:22 +0800

    modify

--- BSP/system.h
+++ BSP/system.h
@@ -1,6 +1,11 @@
 #ifndef __SYSTEM_H
 #define __SYSTEM_H
 
+#include "stm32h7xx_hal.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "Hardware/Inc/lcd.h"
 #include "Hardware/Inc/ltdc_dsi.h"
 
